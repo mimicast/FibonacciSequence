c9d43b720995b27123225334ab4d30291ef0955a
mimicast <47404393+mimicast@users.noreply.github.com>
2023-02-17 13:24:23 -0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,4 @@
 print("Hello! This code will write the first 20 numbers in the Fibonacci Sequence.")
-#print("As an added challenge, it can also start at whatever number you give and print the 20 numbers in the sequence.")
 
 num1 = 0
 num2 = 1
